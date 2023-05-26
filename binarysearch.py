c205d54ded1e43a3c1763d45aad4f39f588a26e3
Pratham Rahane <85764182+Prathamrahane@users.noreply.github.com>
2023-05-26 10:38:58 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,19 +1,25 @@
 def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
+
     while low <= high:
         mid = (low + high) // 2
         guess = arr[mid]
+
         if guess == target:
             return mid
         elif guess < target:
             low = mid + 1
         else:
             high = mid - 1
+
     return -1  
-numbers = [1, 4, 10, 26, 33, 36, 42, 75, 91]
-target_number = 33
+
+numbers = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+target_number = 23
+
 result = binary_search(numbers, target_number)
+
 if result != -1:
     print("Target found at index", result)
 else:
